104b1c064e0622ae45e0e5bb209466a156fca1a1
Aaina Vannan <aevannan@stu.naperville203.org>
2018-09-07 17:18:50 +0000

    Starting Parking

---
+++ again another test/Parking.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+/**
+ * Write a description of class Parking here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Parking
+{
+    public static void main (String[] args)
+    {
+        Stack <Integer> dw = new Stack<>();
+        Stack <Integer> street = new Stack<>();
+        
+        //create if menus
+        int choice;
+        char cont = 'y';
+        while (cont == 'y')
+        {
+            System.out.println("Enter a positive number to add a car; negative number to remove a car; or 0 to stop");
+        }
+    }
+}
