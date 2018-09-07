1656f6b7b0fdc79efafea53700ddb5453300dd6a
Aaina Vannan <aevannan@stu.naperville203.org>
2018-09-07 17:00:17 +0000

    Trying here

---
+++ again another test/NewPro.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class NewPro here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class NewPro
+{
+   public NewPro()
+   {
+       System.out.println("KELP");
+   }
+}
---
+++ again another test/Project1.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Project1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Project1
+{
+    public Project1()
+    {
+        System.out.println("this isn't working");
+    }
+    
+    public int junk(int x)
+    {
+        return 0;
+    }
+    
+}
