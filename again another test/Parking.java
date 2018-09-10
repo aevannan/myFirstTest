490f7a0882201abbb0552f7699715f44f5c08372
Aaina Vannan <aevannan@stu.naperville203.org>
2018-09-10 17:19:05 +0000

    Did unique words

--- again another test/Parking.java
+++ again another test/Parking.java
@@ -1,23 +1,96 @@
 import java.util.Stack;
+import java.util.Scanner;
 /**
  * Write a description of class Parking here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Aaina Vannan
+ * Ex 15.10 
  */
 public class Parking
 {
     public static void main (String[] args)
     {
-        Stack <Integer> dw = new Stack<>();
-        Stack <Integer> street = new Stack<>();
+        Stack <Integer> dw = new Stack<>(); //this is the driveway stack
+        //this is the stack where cars are temporarily moved to so that the cars that weren't put in last in dw can be removed
+        Stack <Integer> street = new Stack<>(); 
+        Scanner keyboard = new Scanner(System.in);
         
         //create if menus
-        int choice;
-        char cont = 'y';
-        while (cont == 'y')
+        int choice = 1; //have to change choice in the while 
+        System.out.println("Your integer is your license plate number!!!");
+        while (choice != 0)
         {
-            System.out.println("Enter a positive number to add a car; negative number to remove a car; or 0 to stop");
+            System.out.println();
+            System.out.println("Enter a positive integer to add a car; negative integer to remove a car; or 0 to stop: ");
+            choice = keyboard.nextInt(); //gets choice
+            if (choice > 0) //to add a car
+            {
+                dw.push(choice); //adds the car to the top of the stack 
+                System.out.println("Your car is at " + choice + " in the driveway");
+                System.out.println("This is the driveway: " + dw); //prints the dw stack
+                System.out.println("This is the street: " + street); //prints the street stack
+            }
+            if (choice < 0)
+            {
+                choice = choice * -1; //have to make license plate positive because only positive numbers are in the driveway
+                if (dw.peek() == choice) //checks to see if the top of the stack is the choice
+                {
+                    dw.pop(); //if it is, remove it
+                    System.out.println("Your car at " + choice + " is now removed from the driveway");
+                    System.out.println("This is the driveway: " + dw);
+                    System.out.println("This is the street: " + street);
+                }
+                else if (dw.peek() != choice)
+                {
+                    int len = dw.size(); //length of the driveway stack
+                    while (len > 0) 
+                    {
+                        if (dw.peek() == choice)
+                        {
+                            dw.pop();
+                            while (street.size() > 0)
+                            {
+                                int stemp = street.pop();
+                                if (stemp != choice)
+                                {
+                                    dw.push(stemp); //moves everything from street into driveway
+                                }
+                            }
+                            System.out.println("Your car at " + choice + " is now removed from the driveway");
+                            System.out.println("All the cars in the street were added back");
+                            System.out.println("This is the driveway: " + dw);
+                            System.out.println("This is the street: " + street);
+                            len = 0; //so the while loop ends
+                        }
+                        else
+                        {
+                            int temp = dw.pop();
+                            street.push(temp); //moves temp to street
+                            System.out.println("The car at " + temp + " was temporarily moved to the street");
+                            System.out.println("This is the driveway: " + dw);
+                            System.out.println("This is the street: " + street);
+                            System.out.println();
+                            len--;
+                        }
+                    }
+                    
+                    //if the car was not in the driveway, I move everything from street to dw stack & print the stacks at the end
+                    System.out.println("Your car was not in the driveway!");
+                    while (street.size() > 0)
+                    {
+                        int stemp = street.pop();
+                        if (stemp != choice)
+                        {
+                            dw.push(stemp); //moves everything from street into driveway
+                        }
+                    }
+                    System.out.println("All the cars in the street were added back");
+                    System.out.println("This is the driveway: " + dw);
+                    System.out.println("This is the street: " + street);
+                }
+            }
         }
     }
 }
+
+
---
+++ again another test/UniqueWords.java
@@ -0,0 +1,53 @@
+import java.util.TreeSet;
+import java.util.Scanner;
+/**
+ * Write a description of class UniqueWords here.
+ * 
+ * Aaina Vannan
+ * Ex 15.12
+ */
+public class UniqueWords
+{
+   public static void main (String[] args)
+   {
+      /*//set doesn't take in duplicates
+      try (Scanner in = new Scanner(new File(filename)))
+      {
+         //Creates corresponding keyset
+         Set <Character> words = new TreeSet();
+         
+         //takes the words, cleans them of non-characters, and adds to map
+         while (in.hasNext())
+         {
+            String word;
+            Character c = word.charAt(0);
+            //ArrayList to store temporary values
+            ArrayList<String> tempVal = new ArrayList();
+
+            // Update the map here
+            // Updates the map with old-fashioned code (no merge method)
+            if (keySet.contains(c)){
+                tempVal = alphabetCount.get(c);
+            }
+            tempVal.add(word); //adds the new word to the list. If list DNE, now size of 1
+            Collections.sort(tempVal);
+            alphabetCount.put(c, tempVal);
+
+         }
+
+         //Prints the map
+         for (char key : keySet){
+             System.out.println(key + " : " + alphabetCount.get(key));
+         }
+      }
+      catch (FileNotFoundException e)
+      {
+         System.out.println("Cannot open: " + filename);
+      }
+    }*/
+    
+  
+
+
+   }
+}
