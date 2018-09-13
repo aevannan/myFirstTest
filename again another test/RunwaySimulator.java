698fd94289b69f57abc949af6ffe04d87e54c971
Aaina Vannan <aevannan@stu.naperville203.org>
2018-09-13 18:56:09 +0000

    downloaded airport stuff

---
+++ again another test/RunwaySimulator.java
@@ -0,0 +1,49 @@
+
+import java.util.Queue;
+import java.util.PriorityQueue;
+/**
+   Class for simulating a runway.
+*/
+public class RunwaySimulator
+{
+   //private Queue 
+   /*
+   /**
+      Constructor.
+   
+   public RunwaySimulator()
+   {
+      . . .
+
+   }
+
+   /**
+      Add a flight symbol to the taking off queue.
+      @param flightSymbol the flight symbol.
+   
+   public void addTakeOff(String flightSymbol)
+   {
+      . . .
+
+   }
+
+   /**
+      Add a flight symbol to the landing queue.
+      @param flightSymbol the flight symbol.
+   
+   public void addLanding(String flightSymbol)
+   {
+      . . .
+
+   }
+
+   /**
+      Handle the next action.
+   
+   public void handleNextAction()
+   {
+      . . .
+
+
+   }*/
+}
